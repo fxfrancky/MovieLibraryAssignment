ad626dad38be16954c0e177f28c0eeb993fc42cf
fxowona <fxjonathan@yahoo.com>
2019-10-13 14:11:36 +0200

    my first commit

---
+++ src/main/java/com/assignment/movielibrary/MovieLibraryAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.movielibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibraryAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/movielibrary/MovieLibraryAssignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.assignment.movielibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieLibraryAssignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
